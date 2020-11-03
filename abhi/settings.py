214fe278e538d20760b5fcdcb3023979732900e0
ImLordImpaler <40574240+ImLordImpaler@users.noreply.github.com>
2020-11-03 09:37:05 +0530

    Update settings.py

--- abhi/settings.py
+++ abhi/settings.py
@@ -144,8 +144,7 @@ AWS_DEFAULT_ACL = 'public-read'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR , 'staticfiles/media')
+
 
 
 
